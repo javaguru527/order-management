f3ca47a7e7de48d0adbafb18350b91f8691dc880
java guru <javaguru527@gmail.com>
2020-06-30 08:17:01 -0700

    Changes for configuration for different environments.

--- src/main/java/com/kris/ordermanagement/OrderManagementApplication.java
+++ src/main/java/com/kris/ordermanagement/OrderManagementApplication.java
@@ -2,11 +2,16 @@ package com.kris.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class OrderManagementApplication {
 
     public static void main(String[] args) {
+
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, System.getenv("SPRING_PROFILE"));
+
+
         SpringApplication.run(OrderManagementApplication.class, args);
     }
 
---
+++ src/main/java/com/kris/ordermanagement/Runner.java
@@ -0,0 +1,17 @@
+package com.kris.ordermanagement;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    @Value("${order-management.send-notification}")
+    private boolean sendNotification;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(" The value of send notification is " + sendNotification);
+    }
+}
